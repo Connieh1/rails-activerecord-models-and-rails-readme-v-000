6f69ab4d7ae354a0b3c79f9e026b6bfc18b5040f
Connie Hyman <constance.hyman@mchsi.com>
2019-02-20 07:00:37 +0000

    Automatically backed up by Learn

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe Post do
-  
+
   it 'can be created' do
     post = Post.create!(title: "My title", description: "The post description")
     expect(post).to be_valid
@@ -11,5 +11,5 @@ describe Post do
     post = Post.create!(title: "My title", description: "The post description")
     expect(post.post_summary).to eq("My title - The post description")
   end
-  
+
 end
